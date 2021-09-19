ddba9dc943cdbb1531a908e8478b849495799481
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2021-09-20 00:04:36 +0300

    created dynamic components

--- vue-list/ex1.vue
+++ vue-list/ex1.vue
@@ -1,25 +1,25 @@
 ﻿<template>
-	<div>
-		<header>
-			<h1>
-				<span>Ex.1</span>
-				Scoped slots / functional components
-			</h1>
-		</header>
-		<main>
-			<list title="Simple list" :items="movies">
-				<template #default="{ item }">
-					<list-item-simple :item="item" />
-				</template>
-			</list>
+  <div>
+    <header>
+      <h1>
+        <span>Ex.1</span>
+        Scoped slots / functional components
+      </h1>
+    </header>
+    <main>
+      <list title="Simple list" :items="movies">
+        <template #default="{ item }">
+          <list-item-simple :item="item" />
+        </template>
+      </list>
 
-			<list title="Detailed list" :items="movies">
-				<template #default="{ item }">
-					<list-item-detailed :item="item" />
-				</template>
-			</list>
-		</main>
-	</div>
+      <list title="Detailed list" :items="movies">
+        <template #default="{ item }">
+          <list-item-detailed :item="item" />
+        </template>
+      </list>
+    </main>
+  </div>
 </template>
 
 <script>
@@ -31,27 +31,27 @@ import listItemSimple from './list-item-simple.vue'
 import listItemDetailed from './list-item-detailed.vue'
 
 export default {
-	name: 'app',
+  name: 'app',
 
-	components: {
-		list,
-		//listNav,
-		listItemSimple,
-		listItemDetailed
-	},
+  components: {
+    list,
+    //listNav,
+    listItemSimple,
+    listItemDetailed
+  },
 
-	data() {
-		return {
-			movies: null
-		}
-	},
+  data() {
+    return {
+      movies: null
+    }
+  },
 
-	async mounted() {
-		const res = await fetch('http://react-cdp-api.herokuapp.com/movies?search=&searchBy=title&sortBy=title&sortOrder=asc&offset=0&limit=12')
-		const { data } = await res.json()
+  async mounted() {
+    const res = await fetch('http://react-cdp-api.herokuapp.com/movies?search=&searchBy=title&sortBy=title&sortOrder=asc&offset=0&limit=12')
+    const { data } = await res.json()
 
-		this.movies = data
-		console.log(this.movies)
-	}
+    this.movies = data
+    console.log(this.movies)
+  }
 }
 </script>
